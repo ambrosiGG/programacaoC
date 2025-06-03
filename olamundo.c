b0af7e0162c660b7a9bd899f0995a6165787badb
ambrosiGG <brunogrossl@hotmail.com>
2025-06-02 21:29:34 -0300

    Update and rename olamund.c to olamundo.c

---
+++ olamundo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (){
+  int idade, matricula;
+  float altura;
+  char nome[50];
+
+printf("Digite sua idade: \n");
+scanf("%d", &idade);
+
+printf("Digite sua altura: \n");
+scanf(%f", &altura);
+
+  printf("Digite o seu nome: \n");
+scanf("%s", &nome);
+
+printf("Digiste sua matricula: \n");
+scanf("%d", &matricula);
+
+printf("Nome do aluno: %s - Matrícula: %d", aluno, matricula);
+printf("Idade: %d - Altura: %f", idade, nome);
+
+return 0;
